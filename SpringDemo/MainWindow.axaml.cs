dc64468f696d69e037aa5002ea06227e0a4a8287
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2022-09-16 11:15:07 +0200

    Initial commit

---
+++ SpringDemo/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace SpringDemo
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
